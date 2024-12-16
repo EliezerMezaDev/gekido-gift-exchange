84233abd4bbf1151c6eca58a1f7aa6f57a799e98
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2024-12-16 15:09:06 -0400

    style: framing the photo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -48,10 +48,10 @@ const Home = () => {
     <div className="w-screen h-[700px] lg:h-screen p-4 flex items-center justify-center">
       <section className="p-8 bg-gray-100 rounded border border-gray-300 flex justify-center items-center flex-col">
         <h1 className="text-3xl font-bold lg:text-6xl text-center">
-          Bienvenido, {student?.name}!
+          Bienvenid@, {student?.name}!
         </h1>
         <h2 className="text-xl lg:text-3xl text-center">
-          Tu amigo secreto es:
+          Tu amigo secreto 2024 es:
         </h2>
 
         <Card student={toGive} />
@@ -59,7 +59,7 @@ const Home = () => {
         <div>
           <button
             type="submit"
-            className=" flex w-full justify-center rounded-md bg-gray-100 border border-gray-300 px-3 py-1.5 text-sm/6 font-semibold  text-gray-900 trasition hover:text-red-700 hover:border-red-300 hover:bg-red-100"
+            className=" flex w-full justify-center rounded-md bg-gray-100 border border-gray-300 px-3 py-1.5 text-sm/6 font-semibold  text-gray-900 transition hover:text-red-700 hover:border-red-300 hover:bg-red-100"
             onClick={(e) => backLogin(e)}
           >
             Volver al inicio
@@ -85,7 +85,7 @@ const Card = (props: { student: Gekido.Student | null }) => {
   return (
     <article className="p-8">
       <div className="w-[200px] h-[300px] flex items-center">
-        <div className="p-2 min-h-[200px] rounded border border-gray-300">
+        <div className="p-4 min-h-[200px] rounded border border-red-200 bg-red-100">
           <img
             src={
               photo
@@ -94,7 +94,9 @@ const Card = (props: { student: Gekido.Student | null }) => {
             }
             alt="given_to"
           />
-          <p className="mt-4 text-2xl text-center ">{name}</p>
+          <p className="mt-4 text-2xl text-center font-bold text-red-900">
+            {name}
+          </p>
         </div>
       </div>
     </article>
